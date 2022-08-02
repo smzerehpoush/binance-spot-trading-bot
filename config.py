48e5ec29827f54b2ce0110a3b950ddb67ca060c1
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2022-08-02 17:55:21 +0430

    remove sleep interval: bot should run via crontab

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 WALLET_USAGE_PERCENT = 20.0  # must be between 0.0 and 100.0
 MAXIMUM_NUMBER_OF_API_CALL_TRIES = 5
-SLEEP_INTERVAL = 24 * 60 * 60
 BINANCE_API_TIMEOUT = 30
+TELEGRAM_USER_ID_LIST = []
 ACTIVE_TRADING_SYMBOLS = [
     {'symbol': 'ETH', 'weight': 1},
     {'symbol': 'BTC', 'weight': 1},
